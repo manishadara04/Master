a60f5681caa818941c923de27d01073bedc09e4e
krishan041 <krishandara04@gmail.com>
2025-01-26 02:17:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
 import os
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
