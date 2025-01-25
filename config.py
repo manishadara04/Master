4f56399c37e916f9bf8c527d23905da06df17376
krishan041 <krishandara04@gmail.com>
2025-01-26 01:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", '7929130193:AAFJswIMtSV_CPazyAPKW4BQ58ijov3t-5I')
-    API_ID = int(os.environ.get("API_ID", '28094744'))
-    API_HASH = os.environ.get("API_HASH", 'a75af4285edc7747c57bb19147ca0b9b')
-    AUTH_USER = os.environ.get('AUTH_USERS','5680454765').split(',')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
+    API_ID = int(os.environ.get("API_ID", '27536109'))
+    API_HASH = os.environ.get("API_HASH", 'b84d7d4dfa33904d36b85e1ead16bd63')
+    AUTH_USER = os.environ.get('AUTH_USERS','6428531614').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     WEBHOOK = True  # Don't change this
     PORT = int(os.environ.get("PORT", '8080'))  # Default to 8000 if not set
     HOST = "https://drm-api-six.vercel.app"
-    CREDIT = "🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌"#Here You Can Change with Your Name  or any custom name or title you prefer
+    CREDIT = "SAFAR"#Here You Can Change with Your Name  or any custom name or title you prefer
 
