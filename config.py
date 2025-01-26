575eaa3149b907ad2cf60937457b8f14adda27c2
krishan041 <krishandara04@gmail.com>
2025-01-26 07:42:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ def home():
     return "Hello, Render!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 class Config(object):
