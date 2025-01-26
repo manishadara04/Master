313b3b289cda016677e526f7f5f3cd2d3a42e299
krishan041 <krishandara04@gmail.com>
2025-01-26 11:50:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,5 @@
 import os
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, World!"
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-    
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
     API_ID = int(os.environ.get("API_ID", '27536109'))
