110013ac4a3c0db493624360e6a27d4f9e5aa7f7
krishan041 <krishandara04@gmail.com>
2025-01-26 10:29:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Your service is running!"
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
