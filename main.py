021830fecd7b35b3a6ee97a8a49426bab97c0599
krishan041 <krishandara04@gmail.com>
2025-01-27 15:31:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def home():
 
 # Debug mode is only enabled when running locally
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
 logging.basicConfig(
     level=logging.INFO,    
