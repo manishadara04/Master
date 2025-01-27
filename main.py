a5ecb2385e153a31d0378073d1317b277f6059fa
krishan041 <krishandara04@gmail.com>
2025-01-27 21:42:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import logging
 # Initialize the bot
 bot = Client(
     "bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
 )
 
 # Define aiohttp routes
