eacfa2752597eb6842a06c87f0fea74ae7dad3c6
manishadara04 <manishadara04@gmail.com>
2025-02-04 20:31:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,97 +1,50 @@
+import os
 from pyrogram import Client as bot, idle
-
 import asyncio
-
 import logging
-
-from flask import Flask
-
+from flask import Flask, jsonify
 from config import Config  # Import Config class here
 
-
-
 app = Flask(__name__)
-
 app.config.from_object(Config)  # Apply the config to the app
 
-
-
 @app.route('/')
-
 def home():
-
     return "Hello, World!"
 
-
-
-if __name__ == '__main__':
-
-    app.run(debug=True)app.run
-
-    
-
+# Set up logging
 logging.basicConfig(
-
-    level=logging.INFO,    
-
+    level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
-
     datefmt='%d-%b-%y %H:%M:%S'
-
 )
-
 LOGGER = logging.getLogger(__name__)
-
 LOGGER.info("Live log streaming to telegram.")
 
-
-
 plugins = dict(root="plugins")
+bot_instance = bot(
+    "Bot",
+    bot_token=Config.BOT_TOKEN,
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    sleep_threshold=120,
+    plugins=plugins,
+    workers=10,
+)
 
+async def start_bot():
+    await bot_instance.start()
+    bot_info = await bot_instance.get_me()
+    LOGGER.info(f"<--- @{bot_info.username} Started --->")
+    for user_id in Config.AUTH_USERS:
+        try:
+            await bot_instance.send_message(chat_id=user_id, text=f"__Congrats! You Are DRM member ... if You get any error then contact me -  {Config.CREDIT}__ ")
+        except Exception as e:
+            LOGGER.error(f"Failed to send message to user {user_id}: {e}")
+            continue
+    await idle()
 
-
-if __name__ == "__main__":
-
-    bot = bot(
-
-        "Bot",
-
-        bot_token=Config.BOT_TOKEN,
-
-        api_id=Config.API_ID,
-
-        api_hash=Config.API_HASH,
-
-        sleep_threshold=120,
-
-        plugins=plugins,
-
-        workers=10,
-
-    )
-
-    async def main():
-
-        await bot.start()
-
-        bot_info = await bot.get_me()
-
-        LOGGER.info(f"<--- @{bot_info.username} Started --->")
-
-        for user_id in Config.AUTH_USERS:
-
-            try:
-
-                await bot.send_message(chat_id=user_id, text=f"__Congrats! You Are DRM member ... if You get any error then contact me -  {Config.CREDIT}__ ")
-
-            except Exception as e:
-
-                LOGGER.error(f"Failed to send message to user {user_id}: {e}")
-
-                continue
-
-        await idle()
-
-    asyncio.get_event_loop().run_until_complete(main())
-
-    LOGGER.info("<---🦅 Stopped 💞 --->")
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
+    app.run(host='0.0.0.0', port=5000)
